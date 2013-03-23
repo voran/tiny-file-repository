1c63313fca95428dba8a40018601343844d6041a
Yavor Stoychev <stoychev.yavor@gmail.com>
2013-03-23 19:35:23 +0200

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Muzika::Application.routes.draw do
-  match "/search(.:format)/:query(/:page)(/:results_per_page)" => "application#search"
+  match "/search(.:format)/(:query)(/:page)(/:results_per_page)" => "application#search"
   match "/browse(.:format)(/*dir)" => "application#browse"
 end
