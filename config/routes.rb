a818afd484d971bae677e2620577bebd8b4c64be
Yavor Stoychev <stoychev.yavor@gmail.com>
2013-03-23 23:17:29 +0200

    redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Muzika::Application.routes.draw do
+  get "/search(.:format)/", :to => redirect("/search(.:format)/(:query)")
   match "/search(.:format)/(:query)(/:page)(/:results_per_page)" => "application#search"
   match "/browse(.:format)/(/*dir)" => "application#browse"
-  get "/search(.:format)/", :to => redirect("/search(.:format)/(:query)")
   root :to => redirect("/browse.html/")
 end
