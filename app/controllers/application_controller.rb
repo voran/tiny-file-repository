c115ebe235312eab9b1bf1938064d89d3c4766f6
Yavor Stoychev <stoychev.yavor@gmail.com>
2013-03-23 18:35:54 +0200

    asd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,7 @@ class ApplicationController < ActionController::Base
     swish.close
     results = results.sort
     respond_to do |format|
-      format.html render  :template => "application/search"
       format.json render :json => { :results => results }
-      format.any render :text => "Invalid format", :status => 403 
     end
   end
 
@@ -36,9 +34,7 @@ class ApplicationController < ActionController::Base
     fiels = files.sort
     subdirs = subdirs.sort
     respond_to do |format|
-      format.html
       format.json { render :json => {:subdirs => subdirs, :files => files } }
-      format.any { render :text => "Invalid format", :status => 403 }
     end
     
   end
