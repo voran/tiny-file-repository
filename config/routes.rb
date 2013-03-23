5a63c476821a14025e29afabe6db29ec1ad10114
Yavor Stoychev <stoychev.yavor@gmail.com>
2013-03-23 22:58:16 +0200

    search apge hook-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Muzika::Application.routes.draw do
   match "/search(.:format)/(:query)(/:page)(/:results_per_page)" => "application#search"
   match "/browse(.:format)/(/*dir)" => "application#browse"
-  match "/search.(.:format)/?q=(:query)" => redirect("/search(.:format)/(:query)")
+  post "/search(.:format)/", :to => redirect("/search(.:format)/(:query)")
   root :to => redirect("/browse.html/")
 end
