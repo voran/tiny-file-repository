a2ddc41b8e5de282a3df598cb899eda276b50f87
Yavor Stoychev <stoychev.yavor@gmail.com>
2013-03-23 17:49:08 +0200

    disable format

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Muzika::Application.routes.draw do
-  match "/api/search/:query(/:page)(/:results_per_page)" => "application#search", :format => false
-  match "/api/browse(/*dir)" => "application#browse", :format => false
+  match "/api/search/:query(/:page)(/:results_per_page)" => "application#search" #, :format => false
+  match "/api/browse(/*dir)" => "application#browse" #, :format => false
 end
