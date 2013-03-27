ca8176496fac4d86f840a59eed8210aacd7bead8
Yavor Stoychev <stoychev.yavor@gmail.com>
2013-03-27 19:28:15 +0200

    make search.html the default page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Muzika::Application.routes.draw do
   match "/submit.html" => "application#submit"
   match "/search(.:format)/(:query)(/:page)(/:results_per_page)" => "application#search"
   match "/browse(.:format)/(/*dir)" => "application#browse"
-  root :to => redirect("/browse.html/")
+  root :to => redirect("/search.html/")
 end
