ebf37545c582d9096c1bdef566a03e4c4875f2de
Yavor Stoychev <yavor.stoychev@transmetrics.eu>
2013-11-11 20:49:30 -0800

    upgrade to rails 4.0.1

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Muzika::Application.config.secret_token = '64d9d86665c70687f12ce6c5f32f1501aafbd1f89199814fc2a892cab81c960d4842dc8d7c39847acb16d4ede041e34fb486babc0802e48fe5b069ba01828b5b'
+Muzika::Application.config.secret_key_base = '007b4edb9dd30959a347c8b1ddb6f30c4a7acb3075189058b66099a27310742714fa6702392278bb6d36a0eb5f45c56bfa8e2a35fdb87144aba9aa30dfa8ee7d'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Muzika::Application.routes.draw do
-  match "/search(.:format)/" => "application#search"
-  match "/browse(.:format)/(/*dir)" => "application#browse"
-  match "/index(.:format)" => "application#index"
+  match "/search(.:format)/" => "application#search", via: [:get, :post]
+  match "/browse(.:format)/(/*dir)" => "application#browse", via: :get
+  match "/index(.:format)" => "application#index", via: :get
   root :to => redirect("/search.html/")
 end
